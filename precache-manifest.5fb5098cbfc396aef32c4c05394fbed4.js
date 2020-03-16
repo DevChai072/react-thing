db4d8dd8d7083eb4cc50812f7a223a883591eb11
Devchai072 <devchai072@gmail.com>
2020-03-16 17:32:10 +0700

    Updates

---
+++ precache-manifest.5fb5098cbfc396aef32c4c05394fbed4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cb6b7607666ee03766b505833dcda9a",
+    "url": "/react-thing/index.html"
+  },
+  {
+    "revision": "03d9107e8ae9964c9e35",
+    "url": "/react-thing/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "cc490c6fd23035222d1a",
+    "url": "/react-thing/static/js/2.4e9106ae.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/react-thing/static/js/2.4e9106ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03d9107e8ae9964c9e35",
+    "url": "/react-thing/static/js/main.a59767fa.chunk.js"
+  },
+  {
+    "revision": "753507f3e4ea8efeac6f",
+    "url": "/react-thing/static/js/runtime-main.4fac9e66.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-thing/static/media/logo.5d5d9eef.svg"
+  }
+]);
